47845a55615fb3691252b4a7f4f6dd9d5cff11bc
Alya Zagatskaya <alya.zagacki@gmail.com>
2022-04-10 20:15:58 +0300

    220410_018

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+let numberOfFilms;
+
+function start() {
+	numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+	while (numberOfFilms === '' || numberOfFilms === null || isNaN(numberOfFilms)) {
+		numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+	}
+}
+// start();
+
 const personalMovieDB = {
 	count: numberOfFilms,
 	movies: {},
@@ -9,10 +18,24 @@ const personalMovieDB = {
 	private: false
 };
 
-countReaction();
-addFilm();
+// detectPersonalLevel();
+// rememberMyFilms();
+showMyDB(personalMovieDB.private);
 
-function countReaction() {
+function showMyDB(hidden) {
+	if (!hidden) {
+		console.log("Hidden: false");
+		console.log(personalMovieDB);
+	}
+}
+
+function writeYourGenres() {
+	for (let i = 0; i < 3; i++) {
+		personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i+1}`);
+	}
+}
+
+function detectPersonalLevel() {
 	if (personalMovieDB.count < 10) {
 		console.log("Просмотрено довольно мало фильмов");
 	} else if (10 <= personalMovieDB.count && personalMovieDB.count <= 30) {
@@ -24,14 +47,14 @@ function countReaction() {
 	}
 }
 
-function addFilm() {
-	for (let i = 0; i < 1; i++){
+function rememberMyFilms() {
+	for (let i = 0; i < 1; i++) {
 		const filmName = prompt('Один из последних просмотренных фильмов?', ''),
 			filmRate = +prompt('На сколько оцените его?', '');
 		console.log(`filmName=${filmName}`);
 		console.log(`filmRate=${filmRate}`);
 		if (filmName != null && filmRate != null && filmName != '' &&
-		filmRate != '' && filmName.length <= 50) {
+			filmRate != '' && filmName.length <= 50) {
 			personalMovieDB.movies[filmName] = filmRate;
 			if (confirm("Хотите добавить еще один фильм?")) {
 				i--;
@@ -41,4 +64,3 @@ function addFilm() {
 		}
 	}
 }
-console.log(personalMovieDB);
